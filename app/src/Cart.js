00167a0de65009532515103272dc48e75070fcdb
Piyush Rai <piyushrai2802@gmail.com>
2020-02-29 19:14:53 +0530

    cart added

--- app/src/Cart.js
+++ app/src/Cart.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
-import {View, Text,AsyncStorage} from 'react-native'
-import { Button } from 'native-base';
-import { Ionicons } from '@expo/vector-icons';
-export default class Cart extends Component {
+import React, { Component } from "react";
+import { View, Text, AsyncStorage } from "react-native";
+import { Button } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+import { connect } from "react-redux";
+import * as handler from "./store/actions/main.js";
 
-    constructor(props){
-        super(props)
-        this.state={
-            cartArr:[]
-        }
-        console.log('cart jkajshdkfjhkashd')
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cartArr: []
+    };
+    console.log("cart jkajshdkfjhkashd");
     //     AsyncStorage.getItem('cart')
     //     .then(req => JSON.parse(req))
     //   .then(json => {console.log('lkskdflkj'+json);
@@ -19,69 +21,95 @@ export default class Cart extends Component {
     //   })
     // })
     //   .catch(error => console.log('error!'));
+  }
+
+  render() {
+    let cartArray = [];
+    for (let key in this.props.cart) {
+      if (this.props.cart[key] !== undefined) {
+        cartArray.push(this.props.cart[key]);
+      }
     }
-
-    render() {
-        return (
-            <View
-            style={{
-                flex:1,
-                flexDirection:'column',
-            }}
-            >
-                    
-                        {this.state.cartArr.map((element,index)=>{
-                            return(
-                                <View key={index}>
-                               
-                                    <Text >
-                                      {`\n Product Id: ${ element.id}`} 
-                                      {` \nQuantity: ${element.quantity}`}
-                                    </Text>
-                                    <Ionicons name="md-trash" size={40} color="red"
-                                        onPress={()=>{
-                                            console.log(`delete product ${index}`)
-                                        }}
-                                        style={{
-                                            marginLeft:'80%'
-                                        }}
-                                    />
-                                </View>
-                            )
-                        })}
-
-                        <View>
-                            {this.state.cartArr.length!==0?
-                            
-                            <Button title={'Clear Cart'} 
-                                onPress={async()=>{
-                                    console.log('cart clearing attempted');
-                                    try {
-                                        // await AsyncStorage.removeItem('cart');
-                                        // await AsyncStorage.setItem('isCartEmpty',true)
-                                        this.setState({
-                                            cartArr:[]
-                                        },()=>{
-                                            alert('All items dropped.')
-                                        })
-
-                                        // await AsyncStorage.setItem('cart',JSON.stringify([]))
-                                        return true;
-                                    }
-                                    catch(exception) {
-                                        return false;
-                                    }
-                                }}
-                            />
-
-                            
-                            :<Text>No Items in cart</Text>}
-
-                            
-
-                        
-                        </View>
+    console.log("cart array");
+    console.log(cartArray);
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column"
+        }}
+      >
+        {cartArray.map((element, index) => {
+          return (
+            <View key={index}>
+              <Text>
+                {`\n Product Id: ${element.itemId}`}
+                {` \nQuantity: ${element.quantity}`}
+              </Text>
+              <Ionicons
+                name="md-trash"
+                size={40}
+                color="red"
+                onPress={() => {
+                  console.log(`delete product ${element.itemId}`);
+                  this.props.deleteItem(element.itemId);
+                }}
+                style={{
+                  marginLeft: "80%"
+                }}
+              />
             </View>
-        )
-    }
+          );
+        })}
+
+        <View>
+          {cartArray.length !== 0 ? (
+            <Button
+              title="Clear Cart"
+              onPress={async () => {
+                console.log("cart clearing attempted");
+                try {
+                  // await AsyncStorage.removeItem('cart');
+                  // await AsyncStorage.setItem('isCartEmpty',true)
+                  this.props.clearCart();
+                  this.setState(
+                    {
+                      cartArr: []
+                    },
+                    () => {
+                      alert("All items dropped.");
+                    }
+                  );
+
+                  // await AsyncStorage.setItem('cart',JSON.stringify([]))
+                  return true;
+                } catch (exception) {
+                  return false;
+                }
+              }}
+            />
+          ) : (
+            <Text>No Items in cart</Text>
+          )}
+        </View>
+      </View>
+    );
+  }
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteItem: itemId => dispatch(handler.deleteItem(itemId)),
+    addItem: item => dispatch(handler.addItem(item)),
+    clearCart: () => dispatch(handler.clearCart())
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    counter: state.cart.counter,
+    cart: state.cart.cart,
+    totalPrice: state.cart.totalPrice
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
