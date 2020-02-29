6d6ed051a51424a4f06ebd7173d59b5a06551434
V-Sharma10 <2017ugcs071@nitjsr.ac.in>
2020-03-01 00:17:52 +0530

    history displayed

--- app/src/History.js
+++ app/src/History.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {View, Text, Image } from 'react-native'
+import {View, Text, Image, SafeAreaView, ScrollView, StyleSheet } from 'react-native'
 import Axios from 'axios'
 export default class History extends Component {
     constructor(props){
@@ -21,12 +21,26 @@ export default class History extends Component {
         this.setState({
             loading:false,
             HistoryArr:getHistory.data.data
+        },()=>{
+            console.log('getHistory completed ')
+            console.log(this.state.HistoryArr)
         })
-        console.log(getHistory.data.data);
+        console.log('jkdfkjhas')
+
+        for(let i in this.state.HistoryArr)
+        {
+        console.log(this.state.HistoryArr[i].items);
+        console.log(this.state.HistoryArr[i].unit_prices);
+        console.log(this.state.HistoryArr[i].qty);
+        console.log(this.state.HistoryArr[i].total);
+        console.log('new item')
+    }
+        
     }
     
     render() {
         return (
+            
             <View>
                 {this.state.loading?
                 // <Image
@@ -47,21 +61,75 @@ export default class History extends Component {
                      </Text>
                 </View>
                 :
+
+                <SafeAreaView style={styles.container}>
+                <ScrollView style={styles.scrollView}>git 
                 <View>
 
                     {this.state.HistoryArr.map((element,index)=>{
                         return(
-                            <View key={index}>
+                            <View key={index}
+                            style={{
+                                paddingBottom:10,
+                                paddingLeft:10,
+                                paddingRight:10,
+                                paddingTop:10,
+                                borderBottomColor: 'black',
+                                borderBottomWidth: 1,
+                            }}
+                            >
                                 <Text>
-                                {index}
+                                    {`\nProducts IDs: \n`}
+                                    {element.items.map((e,i)=>{
+                                    return(
+                                        <Text key={i}>
+                                            {`${e}\n`}
+                                        </Text>
+                                    )
+                                    
+                                })}
+
+                                    {'Quantity: \n'}
+                                { element.qty.map((e,i)=>{
+                                    return(
+                                        <Text key={i}>
+                                            {`${e}\n`}
+                                        </Text>
+                                    )
+                                    
+                                })}
+
+                                
+                                    Total: ₹ {element.total/10**15}{'\n'}
+                                    Time Of Purchase: {Date(element.timeOfPurchase)}
+                                
                                 </Text>
                             </View>
                         )
                     })}
 
                 </View>
+
+</ScrollView>
+</SafeAreaView>
+
                 }
             </View>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    //   marginTop: Constants.statusBarHeight,
+    },
+    scrollView: {
+      backgroundColor: 'transparent',
+      marginHorizontal: 20,
+    },
+    text: {
+      fontSize: 42,
+    },
+  });
